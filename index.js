1f4a273892092170af7f66b9c527b063c77eb906
Yolo Holy <hawhaw247active@gmail.com>
2023-08-01 20:59:17 +0630

    using json format and callback hell example

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-// https://jsonplaceholder.typicode.com/todos/1
-
-// console.log(1);
-// console.log(2);
-
-// setTimeout(()=>{
-//     console.log("I am async");
-// },2000)
-
-// console.log(3);
-// console.log(4);
-let request=new XMLHttpRequest();
+// https://jsonplaceholder.typicode.com/todos
+let getToDos=(resource,callback)=>{
+    let request=new XMLHttpRequest();
 
 request.addEventListener("readystatechange",function(){
-    // console.log(request,request.readyState);
     if(request.readyState==4 && request.status==200){
-        //console.log(request.status);
-        console.log(request.responseText);
+        //callback("data response","error response")
+        let data=JSON.parse(request.responseText)
+        callback(data,undefined)
+
     }else if(request.status==404) {
-        console.log("url link is not available");
+        //console.log("url link is not available");
+        callback(undefined,"url link is unavailable")
     }
 })
 
-request.open("GET","https://jsonplaceholder.typicode.com/todos");
+request.open("GET",resource);
 request.send();
 
 console.log("I am first");
+}
+getToDos("todos/peaky.json",(data,err)=>{
+    if(data){
+        console.log(data);
+        getToDos("todos/dream.json",(data,err)=>{
+            if(data){
+                console.log(data);
+                getToDos("todos/brandquotes.json",(data,err)=>{
+                    if(data){
+                        console.log(data);
+                    }
+                })
+            }
+        })
+    }
+    if(err){
+        console.log(err);
+    }
+})
