c78513640324e087637832d0204b522508773722
Yolo Holy <hawhaw247active@gmail.com>
2023-08-01 21:11:11 +0630

    Promises and Promises Chain

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 // https://jsonplaceholder.typicode.com/todos
-let getToDos=(resource,callback)=>{
-    let request=new XMLHttpRequest();
+let getToDos=(resource)=>{
+   
+    return new Promise((resolve,reject)=>{
+        let request=new XMLHttpRequest();
 
-request.addEventListener("readystatechange",function(){
-    if(request.readyState==4 && request.status==200){
-        //callback("data response","error response")
-        let data=JSON.parse(request.responseText)
-        callback(data,undefined)
-
-    }else if(request.status==404) {
-        //console.log("url link is not available");
-        callback(undefined,"url link is unavailable")
-    }
-})
-
-request.open("GET",resource);
-request.send();
-
-console.log("I am first");
-}
-getToDos("todos/peaky.json",(data,err)=>{
-    if(data){
-        console.log(data);
-        getToDos("todos/dream.json",(data,err)=>{
-            if(data){
-                console.log(data);
-                getToDos("todos/brandquotes.json",(data,err)=>{
-                    if(data){
-                        console.log(data);
-                    }
-                })
+        request.addEventListener("readystatechange",function(){
+            if(request.readyState==4 && request.status==200){
+                let datas=JSON.parse(request.responseText)
+                //callback(data,undefined)
+                resolve(datas)
+        
+            }else if(request.status==404) {
+                //callback(undefined,"url link is unavailable")
+                reject("Something went wrong")
             }
         })
-    }
-    if(err){
-        console.log(err);
-    }
+        
+        request.open("GET",resource);
+        request.send();
+    })
+}
+// console.log(getToDos("haw.json"))
+getToDos("todos/peaky.json")
+.then((data)=>{
+    console.log(data);
+    // getToDos("todos/dream.json")
+    // .then((data)=>{
+    //     console.log(data);
+    // })
+    // .catch((err)=>{
+    //     console.log(err.message);
+    // })
+    return getToDos("todos/dream.json")
+})
+.then((data)=>{
+    console.log(data);
+    return getToDos("todos/brandquotes.json")
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((err)=>{
+    console.log(err.message);
 })
