ace3b77d890c7ca83ffee06d3a9256b3e46f7741
Yolo Holy <hawhaw247active@gmail.com>
2023-08-01 21:20:35 +0630

    Fetch api

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
-// https://jsonplaceholder.typicode.com/todos
-let getToDos=(resource)=>{
-   
-    return new Promise((resolve,reject)=>{
-        let request=new XMLHttpRequest();
-
-        request.addEventListener("readystatechange",function(){
-            if(request.readyState==4 && request.status==200){
-                let datas=JSON.parse(request.responseText)
-                //callback(data,undefined)
-                resolve(datas)
-        
-            }else if(request.status==404) {
-                //callback(undefined,"url link is unavailable")
-                reject("Something went wrong")
-            }
-        })
-        
-        request.open("GET",resource);
-        request.send();
+fetch("todos/peaky.json")
+.then((response)=>{
+    // console.log("resolved",response);
+    if(response.status==404){
+        throw new Error("url link is unavailable")
+    }
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+    fetch("todos/brandquotes.json")
+    .then((response)=>{
+        //can write if 404 checking
+        return response.json();
     })
-}
-// console.log(getToDos("haw.json"))
-getToDos("todos/peaky.json")
-.then((data)=>{
-    console.log(data);
-    // getToDos("todos/dream.json")
-    // .then((data)=>{
-    //     console.log(data);
-    // })
-    // .catch((err)=>{
-    //     console.log(err.message);
-    // })
-    return getToDos("todos/dream.json")
-})
-.then((data)=>{
-    console.log(data);
-    return getToDos("todos/brandquotes.json")
-})
-.then((data)=>{
-    console.log(data);
+    .then((data)=>{
+        console.log(data);
+    })
 })
 .catch((err)=>{
     console.log(err.message);
